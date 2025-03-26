530f968c4f45c6f70aa3078e7d8138dea8a90061
evilicebergs <voiced.fair-0a@icloud.com>
2025-03-26 13:54:35 +0200

    Initial Commit

---
+++ CryptApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptApp
+//
+//  Created by Artem Golovchenko on 2025-03-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptApp/CryptAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptAppApp.swift
+//  CryptApp
+//
+//  Created by Artem Golovchenko on 2025-03-26.
+//
+
+import SwiftUI
+
+@main
+struct CryptAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
