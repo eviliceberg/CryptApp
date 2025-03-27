a3ef37af8e001d786a80351817036e3c40764a3c
evilicebergs <voiced.fair-0a@icloud.com>
2025-03-27 18:52:10 +0200

    add Networking Layer

--- CryptApp/Services/CoinDataService.swift
+++ CryptApp/Services/CoinDataService.swift
@@ -21,28 +21,13 @@ final class CoinDataService {
     private func getCoins() {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&sparkline=true&price_change_percentage=24h&precision=2") else { return }
         
-        coinCancellable = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .background))
-            .tryMap({ (data, response) in
-                guard let response = response as? HTTPURLResponse , response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badURL)
-                }
-                return data
-            })
-            .receive(on: DispatchQueue.main)
+        coinCancellable = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { completion in
-                switch completion {
-                case .failure(let error):
-                    print("Error: \(error.localizedDescription)")
-                case .finished:
-                    break
-                }
-            } receiveValue: { [weak self] result in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] result in
                 self?.allCoins = result
                 self?.coinCancellable?.cancel()
-            }
-
+            })
+        
     }
     
 }
---
+++ CryptApp/Utilites/NetworkingManager.swift
@@ -0,0 +1,51 @@
+//
+//  NetworkingManager.swift
+//  CryptApp
+//
+//  Created by Artem Golovchenko on 2025-03-27.
+//
+
+import Foundation
+import Combine
+
+enum NetworkingError: LocalizedError {
+    case badURLResponse(url: URL)
+    case unknown
+    
+    var errorDescription: String? {
+        switch self {
+        case .badURLResponse(url: let url):
+            return "Bad URL response: \(url)"
+        case .unknown:
+            return "Unknown error occured"
+        }
+    }
+}
+
+final class NetworkingManager {
+    
+    static func download(url: URL) -> AnyPublisher<Data, any Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .tryMap( { try handleURLSession(output: $0, url: url) } )
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleURLSession(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse , response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let failure):
+            print(failure.localizedDescription)
+        }
+    }
+    
+}
